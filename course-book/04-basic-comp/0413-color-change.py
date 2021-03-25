0f2657d06b7b466e78918ef41dde3718c0334798
Aaron Snowberger <jekkilekki@gmail.com>
2021-03-25 10:32:07 +0900

    Update 0413-color-change.py

--- course-book/04-basic-comp/0413-color-change.py
+++ course-book/04-basic-comp/0413-color-change.py
@@ -7,7 +7,7 @@ yCrCb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
 hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
 cv2.imshow('gray', gray)
-cv2.imshow('yCrCb', yCrCb)
+cv2.imshow('yCrCb', yCrCb) # luma, red diff, blue diff (https://en.wikipedia.org/wiki/YCbCr)
 cv2.imshow('hsv', hsv)
 
 cv2.waitKey()
