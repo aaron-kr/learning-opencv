8ce1bd42ce43edd454d7dcedb5eb072a5fc48e84
Aaron Snowberger <jekkilekki@gmail.com>
2021-04-07 14:06:10 +0900

    [update] Update with Office Win machine code

---
+++ course-book/04-basic-comp/0421-randu.py
@@ -0,0 +1,21 @@
+# 0421.py
+import cv2
+import numpy as np 
+import time
+
+dst = np.full((512, 512, 3), (255, 255, 255), dtype = np.uint8)
+nPoints = 200
+pts = np.zeros((1, nPoints, 2), dtype = np.uint16)
+
+cv2.setRNGSeed(int(time.ti
+me()))
+cv2.randu(pts, (100,0), (300, 512))
+
+# draw Pts
+for k in range(nPoints):
+    x, y = pts[0,k][:] # pts[0,k,:]
+    cv2.circle(dst, (x,y), radius = 5, color = (0, 255, 0), thickness = -1)
+
+cv2.imshow('dst', dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
